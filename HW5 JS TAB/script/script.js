66c4e3a1df9046fbd47a91521f82d0c4f0fce34f
Igor-GN <ig050778@gmail.com>
2023-01-24 22:04:43 +0200

    HW5 JS corrected

--- HW5 JS TAB/script/script.js
+++ HW5 JS TAB/script/script.js
@@ -1,35 +1,20 @@
 var tabItem = document.querySelectorAll('.tab');
 
-function viewTab1() {
-    tabItem[0].nextElementSibling.style.display = "block";
-    tabItem[0].style.backgroundColor = "white";
-    tabItem[0].style.borderBottom = "1px solid white";
-    tabItem[1].nextElementSibling.style.display = "none";
-    tabItem[1].style.backgroundColor = "#e0e0e0";
-    tabItem[1].style.borderBottom = "1px solid #3f3f3f";
-    tabItem[2].nextElementSibling.style.display = "none";
-    tabItem[2].style.backgroundColor = "#e0e0e0";
-    tabItem[2].style.borderBottom = "1px solid #3f3f3f";
+
+for (let i = 0; i < tabItem.length; i++) {
+    tabItem[i].addEventListener('click', viewTab);
 }
-function viewTab2() {
-    tabItem[0].nextElementSibling.style.display = "none";
-    tabItem[0].style.backgroundColor = "#e0e0e0";
-    tabItem[0].style.borderBottom = "1px solid #3f3f3f";
-    tabItem[1].nextElementSibling.style.display = "block";
-    tabItem[1].style.backgroundColor = "white";
-    tabItem[1].style.borderBottom = "1px solid white";
-    tabItem[2].nextElementSibling.style.display = "none";
-    tabItem[2].style.backgroundColor = "#e0e0e0";
-    tabItem[2].style.borderBottom = "1px solid #3f3f3f";
+function viewTab() {
+    var open = this.nextElementSibling.style.display = 'block';
+    for (let i = 0; i < tabItem.length; i++) {
+        if (tabItem[i] != open) {
+            tabItem[i].nextElementSibling.style.display = 'none';
+            tabItem[i].style.backgroundColor = "#e0e0e0";
+            tabItem[i].style.borderBottom = "1px solid #3f3f3f"
+        }
+    }
+    this.nextElementSibling.style.display = 'block';
+    this.style.backgroundColor = 'white';
+    this.style.borderBottom = "1px solid white";
 }
-function viewTab3() {
-    tabItem[0].nextElementSibling.style.display = "none";
-    tabItem[0].style.backgroundColor = "#e0e0e0";
-    tabItem[0].style.borderBottom = "1px solid #3f3f3f";
-    tabItem[1].nextElementSibling.style.display = "none";
-    tabItem[1].style.backgroundColor = "#e0e0e0";
-    tabItem[1].style.borderBottom = "1px solid #3f3f3f";
-    tabItem[2].nextElementSibling.style.display = "block";
-    tabItem[2].style.backgroundColor = "white";
-    tabItem[2].style.borderBottom = "1px solid white";
-}
+

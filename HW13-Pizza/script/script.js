e7dc91eb208d5b03818a8929e805aa6c85d4138a
Igor-GN <ig050778@gmail.com>
2023-05-02 10:50:57 +0300

    HW-Pizza-corrected

--- HW13-Pizza/script/script.js
+++ HW13-Pizza/script/script.js
@@ -42,7 +42,6 @@ let prisePizzaSize = 200;
 const addToOrder = document.querySelector('.add-to-order');
 let filling = '';
 const payableItemTotal = document.querySelector('.payable-item-total');
-
 let payableItemDiscont = document.querySelector('.payable-item-discont');
 let payableToPay = document.querySelector('.payable-to-pay');
 let amountOfingredient = 0;
@@ -51,6 +50,15 @@ const userCheck = document.querySelector('.user-check');
 const cuponCode = document.querySelector('.cupon-code');
 const modaWindowCuponSuccessfully = document.querySelector('.modal-window-cupon-successfully');
 const cuponSuccessfullyBtn = document.querySelector('.cupon-successfully-btn');
+const reservationInpName = document.querySelector('#reservation-inp-name');
+const reservationInpEmail = document.querySelector('#reservation-inp-email');
+const InpSubmit = document.querySelector('.InpSubmit');
+const formInfoClient = document.querySelector('.reservation-info-client');
+const clientReservationDate = document.getElementById('clientReservationDate');
+const reservationTime = document.getElementById("appt");
+let validDateRun = 0;
+const ingredientImg = [sousegeimage, baconImage, mushroomImage, octopusImage, tomatoImage, olivesImage, pepperImage, prawnImage, onionImage, ruccolaImage, mintImage];
+const ingredientBtn = ['sousege', 'bacon', 'mushroom', 'octopus', 'tomato', 'olives', 'pepper', 'prawn', 'onion', 'ruccola', 'mint'];
 
 nightMode.addEventListener('click', nightModeSwith);
 cuponSuccessfullyBtn.addEventListener('click', () => {
@@ -59,6 +67,22 @@ cuponSuccessfullyBtn.addEventListener('click', () => {
 getDiscont.addEventListener('click', getCoupon);
 refuseDiscont.addEventListener('click', canselCoupon);
 addToOrder.addEventListener('click', addToOrderIngedients);
+reservationTime.addEventListener('change', checkReservationTime);
+
+function checkReservationTime() {
+    if (clientReservationDate.value == '') {
+        Swal.fire('Спочатку виберіть дату');
+        reservationTime.value = '';
+        return;
+    }
+
+    let timeValue = reservationTime.value;
+    let a = timeValue.split(':')[0] * 60 + timeValue.split(':')[1] * 1;
+    if (a < 539 || a > 1261) {
+        Swal.fire('Столик можливо зарезервувати з 09:00 до 21:00');
+    }
+}
+
 //функція розрахунку вартості замовлення при збільшенні кількості 
 more.addEventListener('click', () => {
     numberPiecesNow = +numberPieces.textContent + 1;
@@ -71,8 +95,8 @@ more.addEventListener('click', () => {
         payableItemDiscont.innerText = Math.ceil((prisePizzaSize + (amountOfingredient * 10)) * numberPiecesNow * (1 - discount) * 100) / 100;
         payableToPay.innerText = 20 + ((prisePizzaSize + (amountOfingredient * 10)) * numberPiecesNow * discount);
     }
-
 });
+
 //функція розрахунку вартості замовлення при зменшенні кількості
 less.addEventListener('click', () => {
     numberPiecesNow = +numberPieces.textContent - 1;
@@ -84,12 +108,10 @@ less.addEventListener('click', () => {
         payableItemDiscont.innerText = Math.ceil((prisePizzaSize + (amountOfingredient * 10)) * numberPiecesNow * (1 - discount) * 100) / 100;
         payableToPay.innerText = 20 + ((prisePizzaSize + (amountOfingredient * 10)) * numberPiecesNow * discount);
     }
-
 });
+
 //функція додавання вартості власної піци до чеку
 function addToOrderIngedients() {
-
-
     userCheck.classList.remove('hide')
     filling = '';
     amountOfingredient = 0;
@@ -101,7 +123,6 @@ function addToOrderIngedients() {
         }
     }
     if (amountOfingredient < 2) {
-        console.log(amountOfingredient);
         Swal.fire('Виберіть мінімум два інгридієнти!');
     }
     else {
@@ -109,7 +130,6 @@ function addToOrderIngedients() {
         nameCheckPizza.innerText = `${sizePizzaCurrent} Pizza with: ${filling}`;
         prise.innerText = `${(prisePizzaSize + (amountOfingredient * 10)) * discount}`;
         cost = +prise.textContent / discount;
-
         payableItemTotal.innerText = `${(prisePizzaSize + (amountOfingredient * 10)) * discount}`;
         payableItemDiscont.innerText = `${Math.ceil((prisePizzaSize + (amountOfingredient * 10)) * (1 - discount) * 100) / 100}`;
         payableToPay.innerText = `${((prisePizzaSize + (amountOfingredient * 10)) * discount) + 20}`;
@@ -122,40 +142,36 @@ for (let i = 0; i < sizePizza.length; i++) {
     sizePizza[i].addEventListener("change", function () {
         if (sizePizzaSmall.checked == true) {
             for (i = 0; i < imgIngredient.length; i++) {
-                imgIngredient[i].style.setProperty('width', '30vw');
+                imgIngredient[i].style.setProperty('width', '320px');
                 sizePizzaCurrent = 'Small "L"';
                 prisePizzaSize = 200;
                 if (window.innerWidth <= 900) {
-                    ingredientsPizza.style = 'padding-top: 33vw;'
+                    ingredientsPizza.style = 'padding-top: 340px;'
                 }
             }
         }
         else if (sizePizzaMiddle.checked == true) {
             for (i = 0; i < imgIngredient.length; i++) {
-                imgIngredient[i].style.setProperty('width', '36vw');
+                imgIngredient[i].style.setProperty('width', '360px');
                 sizePizzaCurrent = 'Medium "XL"';
                 prisePizzaSize = 250;
                 if (window.innerWidth <= 900) {
-                    ingredientsPizza.style = 'padding-top: 39vw;'
+                    ingredientsPizza.style = 'padding-top: 380px;'
                 }
 
             }
         }
         else if (sizePizzaBig.checked == true) {
             for (i = 0; i < imgIngredient.length; i++) {
-                imgIngredient[i].style.setProperty('width', '42vw');
+                imgIngredient[i].style.setProperty('width', '420px');
                 sizePizzaCurrent = 'Big "XXL"';
                 prisePizzaSize = 300;
                 if (window.innerWidth <= 900) {
-                    ingredientsPizza.style = 'padding-top: 45vw;'
+                    ingredientsPizza.style = 'padding-top: 460px;'
                 }
             }
         }
     });
-}
-
-function sizePizzaSelect() {
-    console.log('hello');
 }
 
 function getCoupon() {
@@ -192,6 +208,7 @@ clientNumber.addEventListener('keypress', () => {
         return clientNumber.value = clientNumber.value.slice(0, 9);
     }
 })
+
 // функція перемикання нічного режиму
 function nightModeSwith() {
 
@@ -224,7 +241,8 @@ function nightModeSwith() {
 }
 
 //accordion
-var listItem = document.querySelectorAll('.accordion');
+const listItem = document.querySelectorAll('.accordion');
+
 for (let i = 0; i < listItem.length; i++) {
     listItem[i].addEventListener('click', showListItem);
 }
@@ -234,48 +252,90 @@ function showListItem() {
 
 for (let i = 0; i < ingredientButton.length; i++) {
     ingredientButton[i].addEventListener('click', showIngredient);
-
 }
-
-
 // функція відображення інгридієнтів на піці-конструктору
 function showIngredient() {
-
     this.classList.toggle('button-pressed');
-    if (this.innerText == 'Sousege') {
-
-        sousegeimage.classList.toggle('hide');
-
-    }
-    else if (this.innerText == 'Mushroom') {
-        mushroomImage.classList.toggle('hide');
-    }
-    else if (this.innerText == 'Bacon') {
-        baconImage.classList.toggle('hide');
-    }
-    else if (this.innerText == 'Octopus') {
-        octopusImage.classList.toggle('hide');
-    }
-    else if (this.innerText == 'Tomato') {
-        tomatoImage.classList.toggle('hide');
-    }
-    else if (this.innerText == 'Olives') {
-        olivesImage.classList.toggle('hide');
-    }
-    else if (this.innerText == 'Pepper') {
-        pepperImage.classList.toggle('hide');
-    }
-    else if (this.innerText == 'Prawn') {
-        prawnImage.classList.toggle('hide');
-    }
-    else if (this.innerText == 'Onion') {
-        onionImage.classList.toggle('hide');
-    }
-    else if (this.innerText == 'Ruccola') {
-        ruccolaImage.classList.toggle('hide');
-    }
-    else if (this.innerText == 'Mint') {
-        mintImage.classList.toggle('hide');
-    }
+    ingredientImg[ingredientBtn.indexOf(this.classList[0])].classList.toggle('hide');
 }
 
+function ValidMail() {
+    const re = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
+    const myMail = document.getElementById('reservation-inp-email').value;
+    const valid = re.test(myMail);
+    if (valid) output = 'Адрес эл. почты введен правильно!';
+    else Swal.fire('Адресa эл. почти введена невірно!');
+    return valid;
+}
+
+function ValidPhone() {
+    const re = /^[\d\+][\d\(\)\ -]{7,10}\d$/;
+    const myPhone = document.getElementById('clientNumber').value;
+    const valid = re.test(myPhone);
+    if (valid) output = 'Номер телефона введен правильно!';
+    else Swal.fire('Номер телефона введен неправильно!');;
+    return valid;
+}
+const mail = document.querySelector('.mail');
+mail.addEventListener('change', ValidMail);
+function ValidMail() {
+    const re = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
+    const myMail = document.getElementById('reservation-inp-email').value;
+    const valid = re.test(myMail);
+    if (valid) output = 'Адрес эл. почты введен правильно!';
+    else output = 'Адрес электронной почты введен неправильно!';
+
+    return valid;
+}
+
+InpSubmit.addEventListener('click', submForm);
+
+function submForm() {
+    if (reservationInpName.value.length <= 2) {
+        Swal.fire("Введіть ім'я!");
+        return;
+    }
+    else if (ValidMail() === false) {
+        Swal.fire('Адресa электронної пошти введена невірно!');
+        return;
+    }
+    else if (ValidPhone() === false) {
+        Swal.fire('Невірно вказано номер телефону!');
+        return;
+    }
+    else if (clientReservationDate.value == '') {
+        Swal.fire('Вкажіть дату на яку Ви плануєте замовити столик');
+        return;
+    }
+    else if (reservationTime.value == '') {
+        Swal.fire('Вкажіть час на який Ви плануєте замовити столик');
+        return;
+    }
+}
+const reservationBlank = document.querySelector('.reservation-blank');
+reservationBlank.addEventListener('click', ValidDate)
+function ValidDate() {
+    if (validDateRun == 0) {
+      const endDateOfTheReservation = new Date(Date.now() + (15 * 24 * 60 * 60 * 1000));
+        let year = String(new Date().getFullYear());
+        let month = String(new Date().getMonth() + 1);
+        if (month.length == 1) {
+            month = 0 + month;
+        }
+        let dateOfMonth = String(new Date().getDay());
+        if (dateOfMonth.length == 1) {
+            dateOfMonth = 0 + dateOfMonth;
+        }
+        let endDateOfTheReservationYear = String(new Date(endDateOfTheReservation).getFullYear());
+        let endDateOfTheReservationMonth = String(new Date(endDateOfTheReservation).getMonth() + 1);
+        if (endDateOfTheReservationMonth.length == 1) {
+            endDateOfTheReservationMonth = 0 + endDateOfTheReservationMonth;
+        }
+        let endDateOfTheReservationDateOfMonth = (String(new Date(endDateOfTheReservation))).slice(8, 10);
+        if (endDateOfTheReservationDateOfMonth.length == 1) {
+            endDateOfTheReservationDateOfMonth = 0 + endDateOfTheReservationDateOfMonth;
+        }
+        validDateRun = 1;
+        return clientReservationDate.min = `${year}-${month}-${dateOfMonth}`, clientReservationDate.max = `${endDateOfTheReservationYear}-${endDateOfTheReservationMonth}-${endDateOfTheReservationDateOfMonth}`;
+    }
+}
